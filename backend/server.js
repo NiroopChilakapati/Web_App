f27e5237132cdf1494d8e4d4d896487c029d018e
Niroop Chilakapati <chilakapatiniroop@gmail.com>
2026-06-11 18:43:30 +0530

    Initial Web_App release

--- backend/server.js
+++ backend/server.js
@@ -43,8 +43,10 @@ mongoose
   .then(() => {
     console.log("MongoDB connected");
 
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
